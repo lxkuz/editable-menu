ba73b61b6b44cfa75b80ba2ec288ccd0299f90ca
Sergey Suprunenko <suprunenko.s@gmail.com>
2014-10-02 22:56:53 +0400

    Move some logic to Office model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,28 +20,13 @@ class ApplicationController < ActionController::Base
   def auto_select_office
     unless session[:office_id]
       user_ip = request.remote_ip
-      if Rails.env.development?
-        user_coordinates = [55.755826, 37.6173] # Moscow coordinates (default)
-      else
-        user_coordinates = Geocoder.coordinates(user_ip)
-      end
 
-      # office_distances = []
-      Office.find_each do |office|
-        office_distances << {
-          id: office.id,
-          distance: Geocoder::Calculations.distance_between(user_coordinates,
-                                                            [office.latitude, office.longitude])
-        }
-      end
+      nearest_office = Office.all
+                                .select { |office| !office.distance_to_user(user_ip).nan? }
+                                .min_by { |office| office.distance_to_user(user_ip) }
 
-      nearest_office = office_distances
-                        .delete_if { |office| office[:distance].nan? || office[:distance].nil? }
-                        .min_by { |office| office[:distance] }
-
-      session[:office_id] = nearest_office[:id] if nearest_office
+      session[:office_id] = nearest_office.try(:id)
     end
-
-    @nearest_office = Office.find_by_id(session[:office_id])
+    @nearest_office = nearest_office || Office.find_by_id(session[:office_id])
   end
 end
