1b8c04a2d0c5d107d5131007a288cb96e1ca6a61
Sergey Suprunenko <suprunenko.s@gmail.com>
2014-10-06 03:45:27 +0400

    [fix] bug Access Denied for custom actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  load_and_authorize_resource :news_item, parent: false
+  load_and_authorize_resource :news_item, parent: false, only: [:index]
   def index
     @news_items = @news_items.limit(4).newest.decorate
   end
