10b028dfabaa20a7fefd07df7b3a8d240b78be51
anton_prokhorov <anton_prokhorov@etb-lab.com>
2014-10-09 09:22:18 +0400

    anotate models

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: content_pages
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  page_url    :string(255)
+#  slug        :string(255)
+#  title       :string(255)
+#  description :text
+#  keywords    :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class ContentPage < ActiveRecord::Base
   extend FriendlyId
   friendly_id :page_url, use: [:slugged, :finders]
