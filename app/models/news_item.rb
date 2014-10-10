da07b3c0391d09da00bb7d484e3972e88ec9260a
anton_prokhorov <anton_prokhorov@etb-lab.com>
2014-10-10 14:38:45 +0400

    not validate user attribute in NewsItem model

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -21,7 +21,7 @@ class NewsItem < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :name, :body, :user, presence: true
+  validates :name, :body, :user ,presence: true
 
   scope :newest,    -> { order(updated_at: :desc)}
   scope :last_news, ->(news_id) { where.not(id: news_id).newest.limit(6) }
