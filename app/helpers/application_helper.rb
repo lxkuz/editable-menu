0e7dbcbfb4084a2ac18ab08c63b192020d62a9c6
Tkachenko <supadm.fs@gmail.com>
2014-10-13 13:48:04 +0300

    news edit inline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,8 +18,17 @@ module ApplicationHelper
   def cksubmit(obj)
     if current_user.try(:admin?)
       content_tag :div, id: :'ck-actions' do
-        button_tag(:Сохранить, id: 'ck-submit', data:{ url: url_for([:update_in_place, :admin, obj]), name: obj.class.to_s.downcase}) + button_tag(:Отменить, id: 'ck-cancel')
+        button_tag(:Сохранить, id: 'ck-submit', data:{ url: url_for([:update_in_place, :admin, obj]), name: obj_name(obj)}) + button_tag(:Отменить, id: 'ck-cancel')
       end
+    end
+  end
+
+  def obj_name(obj)
+    #obj can be decorator
+    if obj.class.superclass == Draper::Decorator
+      obj.object.class.to_s.underscore
+    else
+      obj.class.to_s.underscore
     end
   end
 end
