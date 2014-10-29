c2fd5581b11da1704032c1b4b4ccdbcbaebdaa4e
Alexey <kuznetsov@mystand.ru>
2014-10-29 10:50:37 +0600

    non js menu render

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,12 +40,23 @@ module ApplicationHelper
 
   def menu_section name, options = {}
     klass = options[:class]
-    content_tag :div, "",
-                class: klass, "menu-editor-role" => true,
-                data: {menu: name, search_url: search_menu_items_path,
-                       ul_class: options[:ul_class], li_class: options[:li_class],
-                       editable: current_user.try(:admin?)}
-
+    if current_user.try(:admin?)
+      content_tag :div, "",
+                  class: klass, "menu-editor-role" => true,
+                  data: {menu: name, search_url: search_menu_items_path,
+                         ul_class: options[:ul_class], li_class: options[:li_class],
+                         editable: current_user.try(:admin?)}
+    else
+      content_tag :div, class: "#{klass} menu-editor" do
+        content_tag :ul, class: options[:ul_class] do
+          MenuItem.where(menu: name).map do |menu_item|
+            content_tag :li, class: options[:li_class] do
+              concat link_to( menu_item.name, menu_item.url || url_for(menu_item.target))
+            end
+          end.join.html_safe
+        end
+      end
+    end
   end
 
   def menu_editor_btn
