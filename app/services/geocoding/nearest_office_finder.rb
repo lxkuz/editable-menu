39aaa8771e57a3b2da073e5030bd7f46678b571d
Alexey <kuznetsov@mystand.ru>
2014-10-29 11:21:27 +0600

    resque + timeout for ipgeobase request

--- app/services/geocoding/nearest_office_finder.rb
+++ app/services/geocoding/nearest_office_finder.rb
@@ -1,6 +1,12 @@
+require 'timeout'
+
 class Geocoding::NearestOfficeFinder
   def initialize(user_ip)
-    @user_ip = user_ip
+    @user_ip = "188.226.21.160"
+    # puts "WAWWWWWWW"
+    # puts(@user_ip)
+    # puts(@user_ip.class.name)
+
   end
 
   def nearest_office
@@ -10,11 +16,15 @@ class Geocoding::NearestOfficeFinder
   private
 
   def distance_to_user_from(office)
-    ipgeobase_result = Ipgeobase.lookup(@user_ip)
-
-    # If User IP address wrong, use Moscow center coordinates
-    user_coordinates = [ipgeobase_result.lat || 55.75, ipgeobase_result.lng || 37.62]
-
-    office.distance_to(user_coordinates)
+    begin
+        Timeout::timeout(200) do
+          ipgeobase_result = Ipgeobase.lookup(@user_ip)
+          user_coordinates = [ipgeobase_result.lat || 55.75, ipgeobase_result.lng || 37.62]
+          return office.distance_to(user_coordinates)
+        end
+    rescue
+      user_coordinates = [55.75, 37.62]
+      return office.distance_to(user_coordinates)
+    end
   end
 end
