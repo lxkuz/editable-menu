2dc3c9beb19c1b92c16ab1ea99a6ca5aacbc5bce
Tkachenko <supadm.fs@gmail.com>
2014-11-10 13:36:36 +0200

    rm table col

--- public/ckeditor/templates_config.js
+++ public/ckeditor/templates_config.js
@@ -3,4 +3,4 @@
  For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 CKEDITOR.addTemplates("default",{imagesPath:CKEDITOR.getUrl(CKEDITOR.plugins.getPath("templates")+"templates/images/"),templates:[{title:"2 колонки",image:"template2.gif",description:"Шаблон добавляет 2 колонки с заголовком и текстом.",
-    html:'<table cellspacing="0" cellpadding="0" style="width:100%" border="0"><tr><td style="width:50%"><h3>Заголовок 1</h3></td><td></td><td style="width:50%"><h3>Заголовок 2</h3></td></tr><tr><td>Текст 1</td><td></td><td>Текст 2</td></tr></table><p>Больше текста тут.</p>'}]});
+    html:'<table cellspacing="0" cellpadding="0" style="width:100%" border="0"><tr><td style="width:50%"><h3>Заголовок 1</h3></td><td style="width:50%"><h3>Заголовок 2</h3></td></tr><tr><td>Текст 1</td><td>Текст 2</td></tr></table><p>Больше текста тут.</p>'}]});
