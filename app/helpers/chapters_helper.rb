c8c213cf64d701ad5d2ebeea5df1588fbb67acd4
Tkachenko <supadm.fs@gmail.com>
2014-11-13 13:20:23 +0200

    front page hot fixes

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,6 +1,6 @@
 module ChaptersHelper
   def chapter_parent_path(parent, ops)
-    article_path(parent, ops) if parent.is_a?(Article)
-    content_page_path(parent, ops) if parent.is_a?(ContentPage)
+    return article_path(parent, ops) if parent.is_a?(Article)
+    return content_page_path(parent, ops) if parent.is_a?(ContentPage)
   end
 end
