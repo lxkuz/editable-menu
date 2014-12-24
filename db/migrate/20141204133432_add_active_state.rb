03ae0ce1f68fd5f83184c384bffb2c43675f22c4
Sergey Suprunenko <suprunenko.s@gmail.com>
2014-12-24 20:01:14 +0300

    [bugfix] Disable stupid active-office migration

--- db/migrate/20141204133432_add_active_state.rb
+++ db/migrate/20141204133432_add_active_state.rb
@@ -1,6 +1,6 @@
 class AddActiveState < ActiveRecord::Migration
   def change
-    add_column :offices, :active, :boolean, default: true
+    # add_column :offices, :active, :boolean, default: true
     add_column :news_items, :active, :boolean, default: true
     add_column :content_pages, :active, :boolean, default: true
     add_column :articles, :active, :boolean, default: true
